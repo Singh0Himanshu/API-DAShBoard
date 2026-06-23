9ea9e7356a5b2e68eb4569c14e69312c04801b67
Singh0Himanshu <shimanshu.3689@gmail.com>
2026-06-23 22:24:34 +0530

    chore: add sass-embedded as a dev dependency in package.json

--- dashboard/src/api/api.js
+++ dashboard/src/api/api.js
@@ -3,7 +3,8 @@ import axios from 'axios';
 const API_BASE_URL = import.meta?.env?.VITE_API_BASE_URL ?? '/api';
 
 const api = axios.create({
-    baseURL: API_BASE_URL,
+    // baseURL: API_BASE_URL,
+    baseURL: "http://localhost:5000/api",
     headers: {
         'Content-Type': 'application/json',
     },
